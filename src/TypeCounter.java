a51fc1062465ed489fc94fc6fbeed5fcba032c03
DESKTOP-F9095HL\Zahra <zahra.alibrahim@gmail.com>
2018-03-07 00:09:18 -0700

    initial code for loading files

--- src/TypeCounter.java
+++ src/TypeCounter.java
@@ -6,18 +6,90 @@
  * @author 
  *
  */
+import java.awt.List;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class TypeCounter {
 	
-	/**
-	 * This method extracts .java files from the directory and returns 
-	 * source unit to parse.
-	 * 
-	 * @author 
-	 */
-	public void extractJavaFiles() {
+	
+	/***
+ 	* @param directoryPath
+ 	* @return an array of strings with the paths of .java files in directoryPath
+ 	* @throws FileNotFoundException
+ 	* @throws IOException
+ 	* @author Zahra Al Ibrahim
+ 	*/
+	public static String[] getFilesPaths(String directoryPath) throws FileNotFoundException, IOException
+	{
+		File dir = new File(directoryPath);
+	
+		File[] files = dir.listFiles();
+		String filePath = null;
+		String fileName = null;
+		
+		 int javaFileCounter = 0; 
+		 for (File f : files) 
+		 {
+			 fileName = f.getName();
+			 
+			 if(f.isFile() && fileName.endsWith(".java"))
+			 {
+				 javaFileCounter += 1; 
+			 }
+		
+		 }
+		 
+		 String[] paths = new String[javaFileCounter];
+		 int i = 0;
+		 for (File f : files) 
+		 {
+			 filePath = f.getAbsolutePath();
+			 fileName = f.getName();
+			 
+			 if(f.isFile() && fileName.endsWith(".java"))
+			 {
+				 paths[i] = filePath;
+				 i = i + 1;
+			 }
+		
+		 }
+		 
+		 return paths;
+	}
+
+
+
+	/***
+ 	* 
+ 	* @param filePath
+ 	* @return the content of file as a string
+ 	* @throws FileNotFoundException
+ 	* @throws IOException
+ 	* @author Zahra Al Ibrahim
+ 	*/
+	public static String getFileContent(String filePath) throws FileNotFoundException, IOException
+	{
+		BufferedReader buff = new BufferedReader(new FileReader(filePath));
+		StringBuilder builder = new StringBuilder();
+		String line = buff.readLine();
+		while(line != null)
+		{
+			builder.append(line);
+			builder.append(System.lineSeparator());
+			line = buff.readLine();
+		
+		}
+	
+		String fileContent = builder.toString();
+		return fileContent;
 		
 	}
 	
+		
 	/**
 	 * This method parses java source file and creates AST and returns 
 	 * the tree.
@@ -47,7 +119,16 @@ public class TypeCounter {
 	 * @author
 	 * @param args
 	 */
-	public static void main(String[] args) {
+	public static void main(String[] args) throws FileNotFoundException, IOException {
+		
+		// testing getFilePaths and getFileContent
+		String[] paths = getFilesPaths("C:\\Users\\Zahra\\Desktop\\Java\\Pong");
+		String content = null;
+		for(int i=0 ; i < paths.length; i++)
+		{
+		 	content = getFileContent(paths[i]);
+		 	//System.out.println(content);
+		}
 		
 	}
 
