45a927283eb055829d5056a48cdb06d87ba1c29a
kimgahyung <kimgahyung@gmail.com>
2018-03-06 18:54:32 -0700

    Comment: Structure for the project

---
+++ src/TypeCounter.java
@@ -0,0 +1,54 @@
+/**
+ * This class takes a pathname and name of Java Type and counts the number 
+ * of declarations of that type within that directory and counts the number 
+ * of references to each occurrence of that type within that directory.
+ * 
+ * @author 
+ *
+ */
+public class TypeCounter {
+	
+	/**
+	 * This method extracts .java files from the directory and returns 
+	 * source unit to parse.
+	 * 
+	 * @author 
+	 */
+	public void extractJavaFiles() {
+		
+	}
+	
+	/**
+	 * This method parses java source file and creates AST and returns 
+	 * the tree.
+	 * 
+	 * @author
+	 */
+	public void parseFiles() {
+		
+	}
+	
+	/**
+	 * This method takes AST and counts the number of declarations of that 
+	 * type with the given directory and counts the number of references to 
+	 * each occurrences of that type within that directory. 
+	 * And print to the console.
+	 * 
+	 * @author
+	 */
+	public void count() {
+		
+	}
+	
+	/**
+	 * Main method for the class TypeCounter. 
+	 * Takes user inputs for the pathname and the type.
+	 * 
+	 * @author
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+	}
+
+}
