05b2c2d80995a1e7ed38eb107e27e9bef9f8ff24
DESKTOP-F9095HL\Zahra <zahra.alibrahim@gmail.com>
2018-03-09 20:45:08 -0700

    added countJavaFileMethod and removed some comments

--- src/TypeCounter.java
+++ src/TypeCounter.java
@@ -1,25 +1,17 @@
 
-import org.eclipse.jdt.core.IMethod;
 import org.eclipse.jdt.core.JavaCore;
 import org.eclipse.jdt.core.dom.AST;
 import org.eclipse.jdt.core.dom.ASTParser;
 import org.eclipse.jdt.core.dom.ASTVisitor;
 import org.eclipse.jdt.core.dom.AnnotationTypeDeclaration;
 import org.eclipse.jdt.core.dom.CompilationUnit;
-import org.eclipse.jdt.core.dom.CreationReference;
 import org.eclipse.jdt.core.dom.EnumDeclaration;
-import org.eclipse.jdt.core.dom.ExpressionMethodReference;
-import org.eclipse.jdt.core.dom.IMethodBinding;
 import org.eclipse.jdt.core.dom.ITypeBinding;
-import org.eclipse.jdt.core.dom.MemberRef;
-import org.eclipse.jdt.core.dom.MethodInvocation;
-import org.eclipse.jdt.core.dom.MethodRef;
+import org.eclipse.jdt.core.dom.PrimitiveType;
 import org.eclipse.jdt.core.dom.QualifiedName;
 import org.eclipse.jdt.core.dom.SimpleName;
 import org.eclipse.jdt.core.dom.TypeDeclaration;
-import org.eclipse.jdt.core.dom.TypeDeclarationStatement;
 
-import java.awt.List;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -30,37 +22,26 @@ import java.util.Scanner;
 
 public class TypeCounter
 {
-
-
 		
 		static int declarationCounter;
 		static int referenceCounter ;
-		//static int exampleCounter = 0;
-		static int javaFilesCounter ;
 		
 		// Constructor
 		TypeCounter() {
 			declarationCounter = 0;
 			referenceCounter = 0;
-			javaFilesCounter = 0;
 		}
-
 		
-		/**
-		 * This method gets directory as an argument and returns an array of strings with the path 
-		 * of .java files in directory.
-		 * @param directoryPath
-		 * @return an array of strings with the paths of .java files in directoryPath
-		 * @throws FileNotFoundException
-	 	 * @throws IOException
-	 	 * @author Zahra Al Ibrahim
-	 	 */
-		public String[] getFilesPaths(String directoryPath) throws FileNotFoundException, IOException
+		/***
+		 * This method takes the directory path as an argument and counts the number
+		 * of .java files in that directory
+		 */
+		public int countJavaFiles(String directoryPath) throws FileNotFoundException, IOException
 		{
-			File dir = new File(directoryPath);
-		
+			int javaFilesCounter = 0;
+			File dir = new File(directoryPath);			
 			File[] files = dir.listFiles();
-			String filePath = null;
+			
 			String fileName = null;
 			
 			
@@ -74,8 +55,31 @@ public class TypeCounter
 				 }
 			
 			 }
+			 return javaFilesCounter;
+			
+			
+		}
+
+		
+		/**
+		 * This method gets directory as an argument and returns an array of strings with the path 
+		 * of .java files in directory.
+		 * @param directoryPath
+		 * @return an array of strings with the paths of .java files in directoryPath
+		 * @throws FileNotFoundException
+	 	 * @throws IOException
+	 	 * @author Zahra Al Ibrahim
+	 	 */
+		public String[] getFilesPaths(String directoryPath, int javaFiles) throws FileNotFoundException, IOException
+		{
+			File dir = new File(directoryPath);
+		
+			File[] files = dir.listFiles();
+			String filePath = null;
+			String fileName = null;
+			
 			 
-			 String[] paths = new String[javaFilesCounter];
+			 String[] paths = new String[javaFiles];
 			 int i = 0;
 			 for (File f : files) 
 			 {
@@ -94,7 +98,7 @@ public class TypeCounter
 		}
 		
 		
-		public String[] getFilesNames(String directoryPath) throws FileNotFoundException, IOException
+		public String[] getFilesNames(String directoryPath , int javaFiles) throws FileNotFoundException, IOException
 		{
 			File dir = new File(directoryPath);
 		
@@ -102,7 +106,7 @@ public class TypeCounter
 			String fileName = null;
 			
 			 
-			 String[] names = new String[javaFilesCounter];
+			 String[] names = new String[javaFiles];
 			 int i = 0;
 			 for (File f : files) 
 			 {
@@ -118,16 +122,7 @@ public class TypeCounter
 			 
 			 return names;
 		}
-		
-		
-		
-		
-		
-
-		
-		
-		
-		
+	
 
 		/**
 		 * This method reads the file and returns the file contents as string.
@@ -159,14 +154,13 @@ public class TypeCounter
 		/**
 		 * This method parses java source file and creates AST and returns the root of AST.
 		 * 
-		 * @author Ga Hyung Kim
+		 * @author Ga Hyung Kim , Zahra Al Ibrahim
 		 * @return cu CompilationUnit of created AST
 		 */
 		public CompilationUnit parseFiles(String sourceString, String fileName, String[] dirPath) 
 		{
 			ASTParser parser = ASTParser.newParser(AST.JLS8);
 			parser.setEnvironment(null, dirPath, null, false);
-			//System.out.println(fileName);
 			parser.setUnitName(fileName);
 			parser.setKind(ASTParser.K_COMPILATION_UNIT);
 			parser.setSource(sourceString.toCharArray());
@@ -203,9 +197,9 @@ public class TypeCounter
 			public boolean visit(AnnotationTypeDeclaration  node) {
 					
 					ITypeBinding iType = node.resolveBinding();
-					String na = iType.getQualifiedName();
+					String name = iType.getQualifiedName();
 					
-					if (na.equals(type))
+					if (name.equals(type))
 					{
 						System.out.println("annotaion");
 						 declarationCounter += 1;
@@ -217,9 +211,9 @@ public class TypeCounter
 				public boolean visit(TypeDeclaration node) {
 					
 					ITypeBinding iType = node.resolveBinding();
-					String na = iType.getQualifiedName();
+					String name = iType.getQualifiedName();
 					
-					if (na.equals(type))
+					if (name.equals(type))
 					{
 						System.out.println("Class or interface");
 						 declarationCounter += 1;
@@ -228,32 +222,29 @@ public class TypeCounter
 					return true;
 				}
 				
-				public boolean visit(MethodInvocation  node) {
-					
-					//IMethodBinding iType = (IMethodBinding) node.resolveMethodBinding();
-				//	String na = ((ITypeBinding) iType).getQualifiedName();
-				//	System.out.println(node)
-					
-					//if (na.equals(type))
+				
+				public boolean visit(QualifiedName node) {
+										
+					if (node.getFullyQualifiedName().equals(type))
 					{
-						
-					//	 declarationCounter += 1;
+						System.out.println("Qualified name");
+						 referenceCounter += 1;
 					}
 					
 					return true;
 				}
 				
 				
-				public boolean visit(QualifiedName node) {
+				public boolean visit(PrimitiveType node) {
 					
-					//ITypeBinding iType =  (ITypeBinding) node.resolveBinding();
-					//String na =  iType.getQualifiedName();
+					ITypeBinding iType = node.resolveBinding();
+					String name = iType.getQualifiedName();
 					
 					
-					if (node.getFullyQualifiedName().equals(type))
+					if (name.equals(type))
 					{
-						System.out.println("Qualified name");
-						referenceCounter += 1;
+						System.out.println("primitive type");
+						 referenceCounter += 1;
 					}
 					
 					return true;
@@ -261,15 +252,11 @@ public class TypeCounter
 			
 				
 				public boolean visit(SimpleName node) {
-					
-					//ITypeBinding iType =  (ITypeBinding) node.resolveBinding();
-					//String na =  iType.getQualifiedName();
-					
-					
+										
 					if (node.getFullyQualifiedName().equals(type))
 					{
-						System.out.println(node.getFullyQualifiedName());
-						referenceCounter += 1;
+						System.out.println("Simple name");
+						 referenceCounter += 1;
 					}
 					
 					return true;
@@ -282,10 +269,9 @@ public class TypeCounter
 				public boolean visit(EnumDeclaration node) {
 					
 					ITypeBinding iType = node.resolveBinding();
-					String na = iType.getQualifiedName();
-					//System.out.println(node.getName());
+					String name = iType.getQualifiedName();
 					
-					if (na.equals(type))
+					if (name.equals(type))
 					{
 						System.out.println("enum");
 						 declarationCounter += 1;
@@ -322,15 +308,15 @@ public class TypeCounter
 			String typeInput = keyboard.next();
 			
 			// testing getFilePaths and getFileContent
-			String[] paths = p.getFilesPaths(pathInput);
-			String[] names = p.getFilesNames(pathInput);
+			int javaFilesCounter = p.countJavaFiles(pathInput);
+			String[] paths = p.getFilesPaths(pathInput, javaFilesCounter );
+			String[] names = p.getFilesNames(pathInput, javaFilesCounter );
 			String content = null;
 			for(int i=0 ; i < paths.length; i++)
 			{
 			 	content = p.getFileContent(paths[i]);
 			 	CompilationUnit cu = p.parseFiles(content, names[i], new String[] {pathInput});
 			 	p.countTypes(cu , typeInput);
-			 	//System.out.println(content);
 			}
 			System.out.println("Declarations: " +declarationCounter);
 			System.out.println("References: " + referenceCounter);
