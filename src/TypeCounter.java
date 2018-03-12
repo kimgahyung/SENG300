b946bbc7d5974e84d2c9873879d50e27badaa8b8
kimgahyung <kimgahyung@gmail.com>
2018-03-12 16:05:10 -0600

    Commnet: Changed constructor to default again

--- src/TypeCounter.java
+++ src/TypeCounter.java
@@ -29,7 +29,7 @@ public class TypeCounter
 		private static int referenceCounter ;
 		
 		// Constructor
-		public TypeCounter() {
+		TypeCounter() {
 			declarationCounter = 0;
 			referenceCounter = 0;
 		}
