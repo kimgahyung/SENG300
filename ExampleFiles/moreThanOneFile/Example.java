883524647424cba8775f6b400fadc54eab0fc315
kimgahyung <kimgahyung@gmail.com>
2018-03-12 18:22:14 -0600

    Comment: Added files

---
+++ ExampleFiles/moreThanOneFile/Example.java
@@ -0,0 +1,28 @@
+public class Example {
+	
+	public int add(int x, int y) {
+		System.out.println("You are adding two numbers");
+		return x + y;
+	}
+	
+	public int sub(int x, int y) {
+		System.out.println("You are subtracting two numbers");
+		return x - y;
+	}
+	
+	public String concatStrings(String a, String b) {
+		return a.concat(b);
+	}
+	
+	public static void main(String[] args) {
+		Example ex = new Example();
+		int sum = ex.add(1, 2);
+		int diff = ex.sub(10, 5);
+		String concat = ex.concatStrings("Hi", " hello");
+		
+		System.out.println(sum);
+		System.out.println(diff);
+		System.out.println(concat);
+	}
+
+}
