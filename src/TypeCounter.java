8fc82599fbd328afe77ac0852909d141fc6e0f4e
tyler.chow <tyler.chow@cs.ucalgary.ca>
2018-03-13 19:22:50 -0600

    Fix: removed accidental duplicate method

--- src/TypeCounter.java
+++ src/TypeCounter.java
@@ -253,15 +253,6 @@ public class TypeCounter
 					return true;
 				}
 							
-				public boolean visit(SimpleName node) {
-										
-					if (node.getFullyQualifiedName().equals(type))
-					{
-						 referenceCounter += 1;
-					}					
-					return true;
-				}
-							
 				public boolean visit(EnumDeclaration node) {
 					
 					ITypeBinding iType = node.resolveBinding();
